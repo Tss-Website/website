21bb5878da76bc49e1c7759dfac8d938db3a84dd
Karry <1450666842@qq.com>
2018-08-31 23:32:34 +0800

    Update go.php

--- go.php
+++ go.php
@@ -1,3 +1,4 @@
+//BETA2.0 尚未完善
 <?php
 define("IN_QKWEB",true);
 require("inc/inc.php");
@@ -5,15 +6,17 @@ $code = $_GET['code'];
 if($code=='kf'){
 	$url = '/error';
 }else{
-	$sql = "select url from qk_url where alias='$code' and iStatus=1";   //当status情况为1
-	$url = $db->getOne($sql);                                           //get 正确sql位置
-        $sql = "select url from qk_url where alias='$code' and iStatus=0";  //当status情况为0
-	$url = '/badnet';}						//跳转badnet
-//添加istatus 使用$sql
-
+	$sql = "select url from qk_url where alias='$code' and iStatus=1";
+	$url = $db->getOne($sql);
+}
 if($url){
 Header("Location:$url\n");
 }else{
-echo '<script language="javascript" type="text/javascript"> window.location.href="http://3mg.top/error"; </script> ';
+//echo '<script language="javascript" type="text/javascript"> window.location.href="http://3mg.top/error"; </script> ';
+$sql = "select url from qk_url where alias='$code' and iStatus=2";
+	$url = '/badnet';
+}
+if($url){
+Header("Location:$url\n");
 }
 ?>
