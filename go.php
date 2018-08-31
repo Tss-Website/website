f653119cc819d5cc07a72c86fb2e2f7e3c466dd7
Karry <1450666842@qq.com>
2018-08-31 22:30:42 +0800

    Update go.php

--- go.php
+++ go.php
@@ -5,10 +5,12 @@ $code = $_GET['code'];
 if($code=='kf'){
 	$url = '/error';
 }else{
-	$sql = "select url from qk_url where alias='$code' and iStatus=1";
-	$url = $db->getOne($sql);
-        $sql = "select url from qk_url where alias='$code' and iStatus=0";
-	$url = '/badnet';}
+	$sql = "select url from qk_url where alias='$code' and iStatus=1";   //当status情况为1
+	$url = $db->getOne($sql);                                           //get 正确sql位置
+        $sql = "select url from qk_url where alias='$code' and iStatus=0";  //当status情况为0
+	$url = '/badnet';}						//跳转badnet
+//添加istatus 使用$sql
+
 if($url){
 Header("Location:$url\n");
 }else{
